8c8d39a676b366d733bbba2d27e580a7dcd770ab
khunzinshine <khunzinshine@gmail.com>
2023-05-22 22:30:35 +0630

    implemented VersionAndDeploy component

---
+++ src/components/skills/VersionAndDeploy.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const VersionAndDeploy = () => {
+  return (
+    <div className='skills__content'>
+      <h3 className='skills__title'>Other Technologies</h3>
+      <div className='skills__box'>
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bxl-gitlab'></i>
+            <div>
+              <h3 className='skills__name'>Git Lab</h3>
+              <span className='skills__level'>Advanced</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bxl-github'></i>
+            <div>
+              <h3 className='skills__name'>Github</h3>
+              <span className='skills__level'>Advanced</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bxl-netlify'></i>
+            <div>
+              <h3 className='skills__name'>Netlify</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bxl-heroku'></i>
+            <div>
+              <h3 className='skills__name'>Heroku</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i class='bx bxl-vimeo'></i>
+            <div>
+              <h3 className='skills__name'>Vercel</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bx-chevron-up-square'></i>
+            <div>
+              <h3 className='skills__name'>Render</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='bx bxl-aws'></i>
+            <div>
+              <h3 className='skills__name'>AWS</h3>
+              <span className='skills__level'>Basic</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i class='uil uil-linux'></i>
+            <div>
+              <h3 className='skills__name'>Linux</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VersionAndDeploy;
