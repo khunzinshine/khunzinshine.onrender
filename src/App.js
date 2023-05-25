12ea7bab55377e17c5ee74bfeb885e22156d1d4a
khunzinshine <khunzinshine@gmail.com>
2023-05-25 21:22:52 +0630

    Imported Testimonial into App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
+import Testimonial from './components/testimonial/testimonial';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonial />
       </main>
     </>
   );
