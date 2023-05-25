e217836e7de06c97f0525ddb685456aaa2093b62
khunzinshine <khunzinshine@gmail.com>
2023-05-25 21:22:32 +0630

    Implemented Testimonial Section

---
+++ src/components/testimonial/Data.jsx
@@ -0,0 +1,18 @@
+import testimonialImage from '../../assets/testimonial1.png';
+
+export const Data = [
+  {
+    id: 1,
+    image: testimonialImage,
+    title: 'Moe Yan Min Thant',
+    description: `He is one of the most valuable people I have ever met (Smart and professional). Experienced, deadline oriented and intelligent person. And great employee with a very strong problem solving skills. He is an asset to any company, highly creative and a very easy-going person to work with. He is willing to deal with tasks that fall outside of your scope of responsibility. Very nice person ,so highly recommended.
+    `,
+  },
+  {
+    id: 2,
+    image: testimonialImage,
+    title: 'Moe Yan',
+    description: `He is one of the most valuable people I have ever met (Smart and professional). Experienced, deadline oriented and intelligent person. And great employee with a very strong problem solving skills. He is an asset to any company, highly creative and a very easy-going person to work with. He is willing to deal with tasks that fall outside of your scope of responsibility. Very nice person ,so highly recommended.
+    `,
+  },
+];
---
+++ src/components/testimonial/testimonial.jsx
@@ -0,0 +1,53 @@
+import './testimonial.css';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { Data } from './Data';
+// import required modules
+import { Pagination } from 'swiper';
+import React from 'react';
+
+const Testimonial = () => {
+  return (
+    <section className='testimonial container section'>
+      <h2 className='section__title'>Testimonial</h2>
+      <span className='section__subtitle'>What they says</span>
+
+      <Swiper
+        className='testimonial__container'
+        loop={true}
+        spaceBetween={24}
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+      >
+        {Data.map(({ id, image, title, description }) => {
+          return (
+            <SwiperSlide className='testimonial__card' key={id}>
+              <img src={image} alt='clients' className='testimonial__img' />
+              <h3 className='testimonial__name'>{title}</h3>
+              <p className='testimonial__description'>{description}</p>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonial;
