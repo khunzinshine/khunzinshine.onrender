a9f5d34efec6805be1aa29bff4d60a543d297626
khunzinshine <khunzinshine@gmail.com>
2023-06-06 19:50:19 +0630

    added portfolio section into App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Portfolio from './components/portfolio/Portfolio';
 import Qualification from './components/qualification/Qualification';
 import ScrollToTop from './components/scrolltop/ScrollToTop';
 import Services from './components/services/Services';
@@ -19,6 +20,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Portfolio />
         {/* <Testimonial /> */}
         <Contact />
         <ScrollToTop />
