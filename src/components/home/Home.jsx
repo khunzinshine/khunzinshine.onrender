7afca8f8bd924186c5216865bb3f93de3f169e85
khunzinshine <khunzinshine@gmail.com>
2023-07-08 13:34:20 +0630

    Update Home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,18 @@
-import './home.scss';
+import "./home.scss";
 
-import Data from './Data';
-import React from 'react';
-import Scroll from './Scroll';
-import Social from './Social';
+import Data from "./Data";
+import ProfileImg from "../../assets/profilePhoto.jpeg";
+import React from "react";
+import Scroll from "./Scroll";
+import Social from "./Social";
 
 const Home = () => {
   return (
-    <section className='home section' id='home'>
-      <div className='home__container container grid'>
-        <div className='home__content grid'>
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
           <Social />
-          <div className='home__img'></div>
+          <img alt="profile" className="home__img" src={ProfileImg} />
           <Data />
         </div>
         <Scroll />
